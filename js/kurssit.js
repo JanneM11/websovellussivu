cfd45f5260b7a92bf56826d0cd20d8e4fdd21f5f
JanneM11 <jannem92@outlook.com>
2025-05-09 12:13:07 +0300

    sivusto tehty

---
+++ js/kurssit.js
@@ -0,0 +1,16 @@
+const btc_span = document.querySelector('#btc')
+const eth_span = document.querySelector('#eth')
+
+const url = 'https://api.coinlore.net/api/ticker/?id=90,80'
+
+
+const getValues = () => {
+    axios.get(url)
+    .then(response =>{
+        const json = response.data
+        console.log(json)
+        btc_span.innerHTML = json[0].price_usd
+        eth_span.innerHTML = json[1].price_usd
+    })
+}
+ getValues()
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Websovellussivu
+## Sivuston toiminta
+Sivustolta löytyy Bitcoinin ja Ethereumin kurssit Yhdysvaltain dollareina, lisäksi lyhyet esittelyt molemmista.
+## Sivuston käyttö
+Sivusto on heti valmis käytettäväksi. Sivusto hakee automaattisesti kurssit ilman API-avainta.
+## Eri kryptovaluutat
+Mikäli haluat vaihtaa sivulla näkyvät kryptovaluutat vaihda id api/ticker/?id=90,80 90 on bitcoin ja 80 ethereum
